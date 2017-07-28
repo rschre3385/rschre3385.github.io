807498b3a0bd0c37a08165bcb7082ab41c29049e
Ryan Schreder <schredermail@yahoo.com>
2017-07-28 18:59:54 -0500

    added structure and comments

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,11 +1,5 @@
 class ImageUploader < CarrierWave::Uploader::Base
-  process :store_dimensions
 
-
-  version :show do
-    process :resize_to_limit => [500, 500]
-    process :store_dimensions
-  end
 
   storage :file
 
@@ -13,12 +7,5 @@ class ImageUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-  private
-
-    def store_dimensions
-      if file && model
-        model.width, model.height = `identify -format "%wx%h" #{file.path}`.split(/x/)
-      end
-    end
 
 end
