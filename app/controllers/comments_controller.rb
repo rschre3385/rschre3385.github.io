d7dfe8f365e8884be69de5bf6ceddb17729fa4f0
Ryan Schreder <schredermail@yahoo.com>
2017-07-28 17:06:00 -0500

    add comments to post

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,9 +2,9 @@ class CommentsController < ApplicationController
 	before_action :authenticate_user!
 	
 	def create
-	  @post = Post.find(params[:post_id)
+	  @post = Post.find(params[:post_id])
 	  @comment = Comment.create(params[:comment].permit(:content))
-	  @comment.user_id = current_user.user_id 
+	  @comment.user_id = current_user.id
 	  @comment.post_id = @post.id 
 
 	  if @comment.save 
